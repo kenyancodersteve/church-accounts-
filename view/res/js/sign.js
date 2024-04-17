9ad384f5b76d44efc5b4ef3107a148759dac3826
kenyancodersteve <116819643+kenyancodersteve@users.noreply.github.com>
2024-04-18 02:12:20 +0300

    Update sign.js

--- view/res/js/sign.js
+++ view/res/js/sign.js
@@ -5,18 +5,15 @@
 auth.onAuthStateChanged(user => {
     const loggedInPage = 'https://baraka.onrender.com/user';
     const loggedOutPage = 'https://baraka.onrender.com/sign';
-    const loggedInPPage = 'https://baraka.onrender.com/'
+    const homePage = 'https://baraka.onrender.com/';
     
-    // Check if the user is already on the logged-in page
+    // Check if the user is already on the intended page
     if (user && window.location.href !== loggedInPage) {
-        // User is signed in, redirect to a logged-in page
+        // User is signed in and not on the logged-in page, redirect to the logged-in page
         window.location.href = loggedInPage;
-    } else if (!user && window.location.href !== loggedOutPage) {
-        // User is signed out, redirect to a logged-out page
+    } else if (!user && window.location.href !== loggedOutPage && window.location.href !== homePage) {
+        // User is signed out and not on the logged-out page or home page, redirect to the logged-out page
         window.location.href = loggedOutPage;
-    } else if (!user && window.location.href !== loggedInPage) {
-        // User is signed out, redirect to a logged-out page
-        window.location.href = loggedInPage;
     }
 });
 
