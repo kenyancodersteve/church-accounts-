da7ecdb4f5556431fa32518a60445035e9fffffa
kenyancodersteve <116819643+kenyancodersteve@users.noreply.github.com>
2024-04-18 00:48:46 +0300

    Update sign.js

--- view/res/js/sign.js
+++ view/res/js/sign.js
@@ -1,4 +1,19 @@
+var permittedUrls = [
+  "https://baraka.onrender.com/",
+  "https://baraka.onrender.com/sign"
+];
 
+window.onload = function() {
+  auth.onAuthStateChanged(user => {
+    if (!user) {
+      var currentUrl = window.location.href;
+      if (!permittedUrls.includes(currentUrl)) {
+        window.location.href = "https://baraka.onrender.com/"; // Redirect to home if URL not permitted
+      }
+    }
+  });
+  console.log("Page loaded");
+};
 
 const permittedUrls = new Set([
   "https://baraka.onrender.com/",
