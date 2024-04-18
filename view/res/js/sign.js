ab17a3b8bf86f7ac773705a6717bcab58541d65e
kenyancodersteve <116819643+kenyancodersteve@users.noreply.github.com>
2024-04-18 03:11:22 +0300

    Update sign.js

--- view/res/js/sign.js
+++ view/res/js/sign.js
@@ -48,60 +48,95 @@ auth.onAuthStateChanged(user => {
           console.error('Logout error:', error);
       });
   });
+
+
   document.addEventListener("DOMContentLoaded", function() {
-      const loginForm = document.getElementById("loginForm");
-      const signupForm = document.getElementById("signupForm");
-      const toggleLink = document.getElementById("toggleForm");
-      const toggleLink2 = document.getElementById("toggleForm2");
-    
-    
-    
-      toggleLink.addEventListener("click", function(event) {
-        if (loginForm.style.display === "none") {
-          loginForm.style.display = "block";
-          signupForm.style.display = "none";
-        } else {
-          loginForm.style.display = "none";
-          signupForm.style.display = "block";
-        }
-      });
-      toggleLink2.addEventListener("click", function(event) {
-        if (loginForm.style.display === "none") {
-          loginForm.style.display = "block";
-          signupForm.style.display = "none";
-        } else {
-          loginForm.style.display = "none";
-          signupForm.style.display = "block";
-        }
-      });
-    
-
-
-
-
-
-
-      loginForm.addEventListener("submit", function(event) {
-        event.preventDefault();     
+    const loginForm = document.getElementById("loginForm");
+    const signupForm = document.getElementById("signupForm");
+    const toggleLink = document.getElementById("toggleForm");
+    const toggleLink2 = document.getElementById("toggleForm2");
   
-        var form = document.getElementById("login");
-        
-        const formData = new FormData(form);
-        const requestData = Object.fromEntries(formData.entries());
-        console.log(formData,'req',requestData)
-        const email =requestData.email 
-        const password =requestData.password
-        console.log(email,password)
+  
+  
+    toggleLink.addEventListener("click", function(event) {
+      if (loginForm.style.display === "none") {
+        loginForm.style.display = "block";
+        signupForm.style.display = "none";
+      } else {
+        loginForm.style.display = "none";
+        signupForm.style.display = "block";
+      }
+    });
+    toggleLink2.addEventListener("click", function(event) {
+      if (loginForm.style.display === "none") {
+        loginForm.style.display = "block";
+        signupForm.style.display = "none";
+      } else {
+        loginForm.style.display = "none";
+        signupForm.style.display = "block";
+      }
+    })
+
+
+    loginForm.addEventListener("submit", function(event) {
+      event.preventDefault();     
+
+      var form = document.getElementById("login");
       
-  // Attempt to log the user in
+      const formData = new FormData(form);
+      const requestData = Object.fromEntries(formData.entries());
+      console.log(formData,'req',requestData)
+      const email =requestData.email 
+      const password =requestData.password
+      console.log(email,password)
+
+    })
+    
+ signupForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      var form = document.getElementById("signup");
+
+      const formData = new FormData(form);
+      const requestData = Object.fromEntries(formData.entries());
+      console.log(formData,'req',requestData)
+
+      fetch('auth/sign', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+    .then(response => {
+        if (response.ok) {
+            // Handle successful signup
+            response.json().then(data => {
+                console.log('Signup successful', data.message);
+                alert(JSON.stringify(data.message)); // Display the response data
+            });
+        } else {
+            // Handle signup failure
+            response.json().then(errorData => {
+                console.error('Signup failed', errorData);
+                alert(JSON.stringify(errorData)); // Display the error data
+            });
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
+});
+    
+
+        // Attempt to log the user in
   auth.signInWithEmailAndPassword(email, password)
     .then((cred) => {
       // Handle successful login
       console.log('LOGIN WAS A SUCCESS ', cred);
       alert('Login successful. Welcome to Baraka App.'); // Alert the user about login success  
-  
-    
-      // Access user data after authentication
+      
+  // Access user data after authentication
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is signed in
@@ -159,7 +194,8 @@ auth.onAuthStateChanged(user => {
                 // User is signed out
                 console.log("User is signed out");
               }
-              })})
+              })
+    })
               .catch(err => {
                 // Handle login failure
                 console.error('LOGIN FAILED:', err);
@@ -168,42 +204,6 @@ auth.onAuthStateChanged(user => {
             })
               
      
-      signupForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        var form = document.getElementById("signup");
-  
-        const formData = new FormData(form);
-        const requestData = Object.fromEntries(formData.entries());
-        console.log(formData,'req',requestData)
-  
-        fetch('auth/sign', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(requestData)
-      })
-      .then(response => {
-          if (response.ok) {
-              // Handle successful signup
-              response.json().then(data => {
-                  console.log('Signup successful', data.message);
-                  alert(JSON.stringify(data.message)); // Display the response data
-              });
-          } else {
-              // Handle signup failure
-              response.json().then(errorData => {
-                  console.error('Signup failed', errorData);
-                  alert(JSON.stringify(errorData)); // Display the error data
-              });
-          }
-      })
-      .catch(error => {
-          console.error('Error:', error);
-      });
-      });
-    })
-
   
     document.getElementById('logoutBtno').addEventListener('click', function() {
       // Firebase logout functionality
@@ -218,3 +218,11 @@ auth.onAuthStateChanged(user => {
       });
   });
   
+
+  
+    
+    
+
+    })
+
+     
