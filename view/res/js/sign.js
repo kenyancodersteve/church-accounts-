bd25cb6f4df731c6ee50b5f613fa8c664839b28e
kenyancodersteve <116819643+kenyancodersteve@users.noreply.github.com>
2024-04-18 23:53:11 +0300

    Update sign.js

--- view/res/js/sign.js
+++ view/res/js/sign.js
@@ -1,19 +1,19 @@
-
-
-
-// Add listener for authentication state changes
-
 // Add listener for authentication state changes
 auth.onAuthStateChanged(user => {
   const loggedInPage = 'https://baraka.onrender.com/user';
   const loggedOutPage = 'https://baraka.onrender.com/sign';
   const homePage = 'https://baraka.onrender.com/';
-  const ADMIN = 'https://baraka.onrender.com/tres';
+  const tres = 'https://baraka.onrender.com/tres';
   const adminEmail = 'stephenndungu96@gmail.com'; // Define admin email here
 
-  if (user && user.email === adminEmail  && window.location.href !== loggedInPage) {
+  // Check if the user is already on the admin dashboard page
+  if (user && user.email === adminEmail && window.location.href === tres) {
+    return; // If already on the admin dashboard, do nothing
+  }
+
+  if (user && user.email === adminEmail && window.location.href !== tres) {
     // User is signed in with admin email, redirect to admin page
-    window.location.href = ADMIN;
+    window.location.href = tres;
   } else if (user && window.location.href !== loggedInPage) {
     // User is signed in and not on the logged-in page, redirect to the logged-in page
     window.location.href = loggedInPage;
@@ -22,9 +22,6 @@ auth.onAuthStateChanged(user => {
     window.location.href = loggedOutPage;
   }
 });
-
-
-
 
 
 // Function to handle form submission (logout)
